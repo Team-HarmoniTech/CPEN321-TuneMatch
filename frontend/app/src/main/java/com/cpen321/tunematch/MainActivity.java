0341a0f678536cb364facc0fad899d72eb9e99ce
CassielJung <eunjijung1101@gmail.com>
2023-10-22 11:11:59 -0700

    navigation bar working

--- frontend/app/src/main/java/com/cpen321/tunematch/MainActivity.java
+++ frontend/app/src/main/java/com/cpen321/tunematch/MainActivity.java
@@ -1,14 +1,80 @@
 package com.cpen321.tunematch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private HomeFragment homeFrag;
+    private RoomFragment roomFrag;
+    private SearchFragment searchFrag;
+    private ProfileFragment profileFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        setFragment(0);
+                        break;
+                    case R.id.navigation_room:
+                        setFragment(1);
+                        break;
+                    case R.id.navigation_search:
+                        setFragment(2);
+                        break;
+                    case R.id.navigation_profile:
+                        setFragment(3);
+                        break;
+                }
+                return true;
+            }
+        });
+        homeFrag = new HomeFragment();
+        roomFrag = new RoomFragment();
+        searchFrag = new SearchFragment();
+        profileFrag = new ProfileFragment();
+        setFragment(0); // Initialize default fragment to home
+    }
+
+    // Switch between fragments
+    private void setFragment(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+
+        switch (n) {
+            case 0:
+                ft.replace(R.id.mainFrame, homeFrag);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.mainFrame, roomFrag);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.mainFrame, searchFrag);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.mainFrame, profileFrag);
+                ft.commit();
+                break;
+        }
     }
 }
