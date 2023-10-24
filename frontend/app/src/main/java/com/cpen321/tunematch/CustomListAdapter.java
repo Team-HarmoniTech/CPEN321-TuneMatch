2f0f2056fd132c9bfe73badd0fa8bd98a804bd17
CassielJung <eunjijung1101@gmail.com>
2023-10-23 17:09:27 -0700

    home fragment has all the listview working; error on the friends activity view when too many elements are in the list and navigation bar not moving when joined to a room

--- frontend/app/src/main/java/com/cpen321/tunematch/CustomListAdapter.java
+++ frontend/app/src/main/java/com/cpen321/tunematch/CustomListAdapter.java
@@ -1,4 +1,5 @@
 // For friend activity list
+// Written by ChatGPT and add changes to fit my purpose
 package com.cpen321.tunematch;
 
 import android.content.Context;
@@ -6,16 +7,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.List;
 
 public class CustomListAdapter extends BaseAdapter {
     private Context context;
+    private String listType;
     private List<List<String>> itemList;            // List = [(f1, s1), (f2, s2) ... ]
 
-    public CustomListAdapter(Context context, List<List<String>> itemList) {
+    public CustomListAdapter(Context context, String listType, List<List<String>> itemList) {
         this.context = context;
+        this.listType = listType;
         this.itemList = itemList;
     }
 
@@ -38,17 +46,48 @@ public class CustomListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.friend_activity_custom, parent, false);
+            if (listType.equals("FriendsList")) {
+                convertView = inflater.inflate(R.layout.friend_activity_custom, parent, false);
+            } else {
+                convertView = inflater.inflate(R.layout.listening_session_custom, parent, false);
+            }
         }
 
         TextView friendNameText = convertView.findViewById(R.id.friendNameText);
         String nameText = itemList.get(position).get(0);
         friendNameText.setText(nameText);
 
-        // Configure other views in the layout as needed
-        TextView songText = convertView.findViewById(R.id.songTitleText);
-        String titleText = itemList.get(position).get(1);
-        songText.setText(titleText);
+        if (listType.equals("FriendsList")) {
+            TextView songText = convertView.findViewById(R.id.songTitleText);
+            String titleText = itemList.get(position).get(1);
+            songText.setText(titleText);
+        }
+
+        if (listType.equals("SessionsList")) {
+            Button joinBtn = convertView.findViewById(R.id.joinBtn);
+            // TODO: Whatever is required to join to existing listening session, need to send info through
+            // TODO: itemlist
+            int sessionId = 0;
+
+            joinBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // TODO: send join request
+                    RoomFragment roomFragment = new RoomFragment();
+
+                    // Get the parent activity's fragment manager
+                    FragmentManager fragmentManager = ((FragmentActivity) context).getSupportFragmentManager();
+
+                    // Begin a fragment transaction
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                    // open room fragment
+                    transaction.replace(R.id.mainFrame, roomFragment);
+                    transaction.addToBackStack(null);       // If you want to add the transaction to the back stack
+                    transaction.commit();
+                }
+            });
+        }
 
         return convertView;
     }
--- frontend/app/src/main/java/com/cpen321/tunematch/HomeFragment.java
+++ frontend/app/src/main/java/com/cpen321/tunematch/HomeFragment.java
@@ -23,14 +23,26 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_home, container, false);
 
+        // Add friends activity
         ListView friendsActivityList = view.findViewById(R.id.friendsList);
-        List<List<String>> itemList = new ArrayList<>();
+        List<List<String>> friendsListItems = new ArrayList<>();
         // TODO: Need to query server to get the list of friends and what they are currently listening
-        for (int i = 0; i < 5; i++) {
-            itemList.add(Arrays.asList(String.format("Item %d", i), String.format("Song %d", i)));
+        for (int i = 0; i < 10; i++) {
+            friendsListItems.add(Arrays.asList(String.format("Item %d", i), String.format("Song %d", i)));
         }
-        CustomListAdapter adapter = new CustomListAdapter(getContext(), itemList);
-        friendsActivityList.setAdapter(adapter);
+        CustomListAdapter friendsAdapter = new CustomListAdapter(getContext(), "FriendsList", friendsListItems);
+        friendsActivityList.setAdapter(friendsAdapter);
+
+        // Add existing listening session
+        ListView sessionList = view.findViewById(R.id.listeningSessionList);
+        List<List<String>> sessionListItems = new ArrayList<>();
+        // TODO: get session IDs of existing sessions to send it to the button onclick handler
+        for (int i = 0; i < 15; i++) {
+            sessionListItems.add(Arrays.asList(String.format("Friends %d", i), "_"));
+        }
+        CustomListAdapter sessionAdapter = new CustomListAdapter(getContext(), "SessionsList", sessionListItems);
+        sessionList.setAdapter(sessionAdapter);
+
 
         return view;
     }
