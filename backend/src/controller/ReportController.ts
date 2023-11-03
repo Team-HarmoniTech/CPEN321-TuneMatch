2d28544b9017a923d9bd137fbfd52c0db26ee78e
Theo Siemens-Rhodes <theo.siemensrhodes@gmail.com>
2023-11-03 14:13:11 -0700

    add reporting

--- backend/src/controller/ReportController.ts
+++ backend/src/controller/ReportController.ts
@@ -4,8 +4,8 @@ import { NextFunction, Request, Response } from "express";
 export class ReportController {
   // ChatGPT usage: No
   async generateReport(req: Request, res: Response, next: NextFunction) {
-    const { offenderId, reporterId, reason, text, context } = req.body;
-    const report = await reportService.generateReport(offenderId, req.currentUserId, {
+    const { offenderId, reason, text, context } = req.body;
+    const report = await reportService.generateReport(offenderId, req.currentUserSpotifyId, {
       reason,
       text,
       context,
