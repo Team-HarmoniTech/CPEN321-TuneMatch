cf1cb36985d7c0727dc7e6f8b3f3ab08c60a8a3a
Theo Siemens-Rhodes <theo.siemensrhodes@gmail.com>
2023-11-03 14:10:28 -0700

    fix report route

--- backend/src/controller/ReportController.ts
+++ backend/src/controller/ReportController.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 
 export class ReportController {
   // ChatGPT usage: No
-  async generate(req: Request, res: Response, next: NextFunction) {
+  async generateReport(req: Request, res: Response, next: NextFunction) {
     const { offenderId, reporterId, reason, text, context } = req.body;
     const report = await reportService.generateReport(offenderId, req.currentUserId, {
       reason,
@@ -14,14 +14,14 @@ export class ReportController {
   }
 
   // ChatGPT usage: No
-  async view(req: Request, res: Response, next: NextFunction) {
+  async viewReports(req: Request, res: Response, next: NextFunction) {
     const { dateFrom, dateTo } = req.query;
     const reports = await reportService.viewReports(dateFrom, dateTo);
     res.send(reports);
   }
 
   // ChatGPT usage: No
-  async ban(req: Request, res: Response, next: NextFunction) {
+  async banUser(req: Request, res: Response, next: NextFunction) {
     const userId = await reportService.banUser(req.params.userId);
     res.send({ message: `User with ID ${userId} has been banned.` });
   }
