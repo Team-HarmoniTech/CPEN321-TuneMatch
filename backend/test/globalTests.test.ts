06b481d2e45528c93fbf5a256960df285f9960b3
Theo Siemens-Rhodes <theo.siemensrhodes@gmail.com>
2023-11-19 15:51:40 -0800

    100% backend coverage

--- backend/test/globalTests.test.ts
+++ backend/test/globalTests.test.ts
@@ -1,20 +1,38 @@
 jest.mock('@src/config', () => ({
   ...jest.requireActual('@src/config'),
-  ENVIRONMENT: 'prod',
+  ENVIRONMENT: 'dev',
 }));
 
-import { ENVIRONMENT } from '@src/config';
+import { ENVIRONMENT, PORT } from '@src/config';
 import { server } from '@src/index';
+import logger from '@src/logger';
 import { startServer } from '@src/startup';
 
 describe('Global Test', () => {
     it('should start the server', async () => {
+        const logSpy = jest.spyOn(console, "log");
         server.close();
-        expect(ENVIRONMENT).toBe('prod');
+        expect(ENVIRONMENT).toBe('dev');
 
         startServer();
 
         expect(server.listening).toBeTruthy();
         await new Promise(f => setTimeout(f, 1000));
+        expect(logSpy).toHaveBeenCalledWith(`Express server has started on port ${PORT}.`);
     });
+
+    it('should log', async () => {
+      const logSpy = jest.spyOn(console, "log");
+      const debugSpy = jest.spyOn(console, "debug");
+      const errSpy = jest.spyOn(console, "error");
+      
+      logger.log("test log");
+      expect(logSpy).toHaveBeenCalledWith("test log");
+
+      logger.dev("test debug");
+      expect(debugSpy).toHaveBeenCalledWith("test debug");
+
+      logger.err("test err");
+      expect(errSpy).toHaveBeenCalledWith("test err");
+  });
 });
